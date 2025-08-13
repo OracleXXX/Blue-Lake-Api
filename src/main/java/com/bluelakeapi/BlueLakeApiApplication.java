0774fcf6bc82f0bb9dd6dba3b82cd9f24464d7c4
Guanxiong Xue <xueguanxiong@mail.com>
2025-08-12 23:29:27 -0500

    init

---
+++ src/main/java/com/bluelakeapi/BlueLakeApiApplication.java
@@ -0,0 +1,13 @@
+package com.bluelakeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueLakeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlueLakeApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bluelakeapi/BlueLakeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluelakeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueLakeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
